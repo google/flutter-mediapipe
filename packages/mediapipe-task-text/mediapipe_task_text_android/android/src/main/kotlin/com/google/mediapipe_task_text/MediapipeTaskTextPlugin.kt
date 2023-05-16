c9239187d56b7ae6182b1b5a2667c93ef466b896
Craig Labenz <craig.labenz@gmail.com>
2023-05-16 15:35:55 -0700

    Add skeleton Kotlin code

--- packages/mediapipe-task-text/mediapipe_task_text_android/android/src/main/kotlin/com/google/mediapipe_task_text/MediapipeTaskTextPlugin.kt
+++ packages/mediapipe-task-text/mediapipe_task_text_android/android/src/main/kotlin/com/google/mediapipe_task_text/MediapipeTaskTextPlugin.kt
@@ -16,6 +16,32 @@ class MediapipeTaskTextPlugin: FlutterPlugin, MethodCallHandler {
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
 
+  private val listener = object :
+    TextClassifierHelper.TextResultsListener {
+    override fun onResult(
+      results: TextClassifierResult,
+      inferenceTime: Long
+    ) {
+      // Instead of popping a bottomsheet - need to return data on the MethodChannel
+      runOnUiThread {
+        activityMainBinding.bottomSheetLayout.inferenceTimeVal.text =
+          String.format("%d ms", inferenceTime)
+
+        adapter.updateResult(results.classificationResult()
+          .classifications().first()
+          .categories().sortedByDescending {
+              it.score()
+          }, classifierHelper.currentModel
+        )
+      }
+    }
+
+    override fun onError(error: String) {
+      // Instead of popping a toast - need to return the error on the MethodChannel
+      Toast.makeText(this@MainActivity, error, Toast.LENGTH_SHORT).show()
+    }
+  }
+
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "mediapipe_task_text")
     channel.setMethodCallHandler(this)
@@ -24,6 +50,12 @@ class MediapipeTaskTextPlugin: FlutterPlugin, MethodCallHandler {
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else if (call.method == "classifyText") {
+      // Need to adapt this `listener` to accept `result` as a parameter to call `result.success`.
+      classifierHelper = TextClassifierHelper(
+          context = this@MainActivity,
+          listener = listener
+      )
     } else {
       result.notImplemented()
     }
