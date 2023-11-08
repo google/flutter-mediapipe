e45639e85f41937198443356b6a85e20c3ba2182
Craig Labenz <craig.labenz@gmail.com>
2023-11-08 14:52:58 -0800

    code review changes

--- packages/mediapipe-task-text/lib/mediapipe_text.dart
+++ packages/mediapipe-task-text/lib/mediapipe_text.dart
@@ -1,3 +1,8 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// Package containing MediaPipe's text-specific tasks.
 library mediapipe_text;
 
 export 'src/tasks/text_classification/text_classifier.dart';
--- packages/mediapipe-task-text/lib/src/tasks/text_classification/containers/text_classifier_result.dart
+++ packages/mediapipe-task-text/lib/src/tasks/text_classification/containers/text_classifier_result.dart
@@ -1,5 +1,9 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:mediapipe_core/mediapipe_core.dart';
-import '../../../third_party/mediapipe/generated/mediapipe_text_bindings.dart'
+import 'package:mediapipe_text/src/third_party/mediapipe/generated/mediapipe_text_bindings.dart'
     as bindings;
 
 /// Container with results of MediaPipe's `classifyText` task.
