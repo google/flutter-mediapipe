806c082df3764983237d797f564eb312d5183627
Craig Labenz <craig.labenz@gmail.com>
2023-11-13 14:58:47 -0800

    remove development logging

--- packages/mediapipe-task-text/build.dart
+++ packages/mediapipe-task-text/build.dart
@@ -7,20 +7,8 @@ const localAssetFilename = 'libtext_classifier.dylib';
 const assetLocation =
     'https://storage.googleapis.com/random-storage-asdf/$cloudAssetFilename';
 
-File outputFile = File(
-    '/Users/craiglabenz/Dev/git/google/flutter-mediapipe/packages/mediapipe-task-text/logs-build.txt');
-
-final logs = <String>[];
-
 void main(List<String> args) async {
-  if (await outputFile.exists()) {
-    await outputFile.delete();
-  }
-  await outputFile.create();
-
-  log(args.join(' '));
   _build(args);
-  outputFile.writeAsString(logs.join('\n'));
 }
 
 Future<void> _build(List<String> args) async {
@@ -29,7 +17,6 @@ Future<void> _build(List<String> args) async {
   final downloadFileLocation = buildConfig.outDir.resolve(localAssetFilename);
   if (!buildConfig.dryRun) {
     final downloadUri = Uri.parse(assetLocation);
-    log('Downloading $downloadUri');
     final downloadResponse = await http.get(downloadUri);
     final downloadedFile = File(downloadFileLocation.toFilePath());
     if (downloadResponse.statusCode == 200) {
@@ -39,8 +26,8 @@ Future<void> _build(List<String> args) async {
       downloadedFile.createSync();
       downloadedFile.writeAsBytes(downloadResponse.bodyBytes);
     } else {
-      log('${downloadResponse.statusCode} :: ${downloadResponse.body}');
-      return;
+      throw Exception(
+          '${downloadResponse.statusCode} :: ${downloadResponse.body}');
     }
   }
   buildOutput.dependencies.dependencies
@@ -55,9 +42,4 @@ Future<void> _build(List<String> args) async {
     ),
   );
   await buildOutput.writeToFile(outDir: buildConfig.outDir);
-  log(buildConfig.outDir.toString());
-}
-
-void log(String val) {
-  logs.add('\n[${DateTime.now().toIso8601String()}] $val\n');
 }
