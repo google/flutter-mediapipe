0df391423fba9ab5ff56f33af7e2d29fec8ea73f
Craig Labenz <craig.labenz@gmail.com>
2024-01-04 13:07:27 -0800

    adds sdks_finder command to builder utility

---
+++ tool/builder/lib/extensions.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'dart:io';
+
+extension EasyOutput on Process {
+  Future<List<String>> get processedStdErr => _process(this.stderr);
+
+  Future<List<String>> get processedStdOut => _process(this.stdout);
+
+  Future<List<String>> _process(Stream<List<int>> stream) async {
+    return utf8.decoder
+        .convert((await stream.toList())
+            .fold<List<int>>([], (arr, el) => arr..addAll(el)))
+        .split('\n');
+  }
+}
+
+/// Returns the last full chunk from a Url-like String.
+///
+/// From "/an/awesome/url/", returns "url".
+/// From "/an/awesome/url", returns "url".
+/// From "/an/awesome/url/", with a depth of 1, returns "awesome"
+/// From "/an/awesome/url", with a depth of 1, returns "awesome"
+String lastChunk(String url, {int depth = 0}) {
+  final indexOffset = (url.endsWith('/')) ? -2 - depth : -1 - depth;
+  final splitUrl = url.split('/');
+  return splitUrl[splitUrl.length + indexOffset];
+}
+
+extension DefaultableMap<K, V> on Map {
+  void setDefault(K key, V def) {
+    if (!containsKey(key)) {
+      this[key] = def;
+    }
+  }
+}
