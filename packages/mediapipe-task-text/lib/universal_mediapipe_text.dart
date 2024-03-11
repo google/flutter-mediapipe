ce4236507fcc895b0e9dcd2f7a6da0f9d9d19d32
Craig Labenz <craig.labenz@gmail.com>
2024-03-11 15:26:34 -0700

    adds text embedding classes to text pkg

--- packages/mediapipe-task-text/lib/universal_mediapipe_text.dart
+++ packages/mediapipe-task-text/lib/universal_mediapipe_text.dart
@@ -127,3 +127,59 @@ class TextEmbedderResult extends BaseEmbedderResult {
   // ignore: must_call_super
   void dispose() => throw UnimplementedError();
 }
+
+/// {@macro TextEmbedder}
+class TextEmbedder extends BaseTextEmbedder {
+  /// {@macro TextEmbedder}
+  TextEmbedder(TextEmbedderOptions options);
+
+  @override
+  Future<TextEmbedderResult> embed(String text) => throw UnimplementedError();
+
+  @override
+  Future<double> cosineSimilarity(Embedding a, Embedding b) =>
+      throw UnimplementedError();
+
+  /// Cleans up all resources.
+  void dispose() {}
+}
+
+/// {@macro TextEmbedderOptions}
+class TextEmbedderOptions extends BaseTextEmbedderOptions {
+  /// {@template TextEmbedderOptions.fromAssetPath}
+  /// Convenience constructor that looks for the model asset at the given file
+  /// system location.
+  /// {@endtemplate}
+  TextEmbedderOptions.fromAssetPath(
+    String assetPath, {
+    EmbedderOptions embedderOptions = const EmbedderOptions(),
+  });
+
+  /// {@template TextEmbedderOptions.fromAssetBuffer}
+  /// Convenience constructor that uses a model existing in memory.
+  /// {@endtemplate}
+  TextEmbedderOptions.fromAssetBuffer(
+    Uint8List assetBuffer, {
+    EmbedderOptions embedderOptions = const EmbedderOptions(),
+  });
+
+  @override
+  BaseOptions get baseOptions => throw UnimplementedError();
+
+  @override
+  EmbedderOptions get embedderOptions => throw UnimplementedError();
+}
+
+/// {@macro TextEmbedderResult}
+class TextEmbedderResult extends BaseTextEmbedderResult {
+  /// {@template TextEmbedderResult.fake}
+  /// Instantiates a [TextEmbedderResult] with fake data for testing.
+  /// {@endtemplate}
+  TextEmbedderResult({required Iterable<Embedding> embeddings});
+
+  @override
+  Iterable<Embedding> get embeddings => throw UnimplementedError();
+
+  @override
+  void dispose() => throw UnimplementedError();
+}
