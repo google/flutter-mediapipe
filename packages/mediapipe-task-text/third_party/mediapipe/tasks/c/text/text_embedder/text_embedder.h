500710f7a696245e74f4d2bafcf9ede55db9dd72
Craig Labenz <craig.labenz@gmail.com>
2024-03-11 15:12:12 -0700

    fixed embedding header file and bindings

--- packages/mediapipe-task-text/third_party/mediapipe/tasks/c/text/text_embedder/text_embedder.h
+++ packages/mediapipe-task-text/third_party/mediapipe/tasks/c/text/text_embedder/text_embedder.h
@@ -72,10 +72,10 @@ MP_EXPORT int text_embedder_close(void* embedder, char** error_msg);
 // 0.
 //
 // [1]: https://en.wikipedia.org/wiki/Cosine_similarity
-MP_EXPORT int text_embedder_cosine_similarity(const Embedding& u,
-                                              const Embedding& v,
+MP_EXPORT int text_embedder_cosine_similarity(const struct Embedding* u,
+                                              const struct Embedding* v,
                                               double* similarity,
-                                              char** error_msg);
+                                              char** error_msg);                                       
 
 #ifdef __cplusplus
 }  // extern C
